557f30dafa6c1db19501a9410dbe28f35e21202a
Mia Allen <mia@vidops.io>
2021-08-09 12:17:43 +0100

    like counter implemented

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,9 +1,19 @@
 import "./post.css";
 import {MoreVert} from "@material-ui/icons";
 import {Users} from "../../dummyData";
+import {useState} from "react";
+
 
 
 export default function Post({post}) {
+    const [like, setLike] = useState(post.like)
+    const [isLiked, setIsLiked] = useState(false)
+
+
+    const likeHandler =()=>{
+        setLike(isLiked ? like-1 : like+1)
+        setIsLiked(!isLiked)
+    }
     return (
         <div className="post">
             <div className="postWrapper">
@@ -23,9 +33,9 @@ export default function Post({post}) {
                    </div>
                 <div className="postBottom">
                    <div className="postBottomLeft">
-                    <img className="likeIcon" src="assets/like.png" alt=""/>
-                    <img className="likeIcon" src="assets/heart.png" alt=""/>
-                    <span className="postLikeCounter">{post.like} like it</span>
+                    <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt=""/>
+                    <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt=""/>
+                    <span className="postLikeCounter">{like} like it</span>
                     </div>
                    <div className="postBottomRight">
                        <span className="postCommentText">{post.comment} comments</span>

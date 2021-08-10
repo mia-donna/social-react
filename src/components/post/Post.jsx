3af34e1cc4e3dea8461d60485008b1fb96ff305c
Mia Allen <mia@vidops.io>
2021-08-10 18:13:50 +0100

    add link for profile image

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -3,6 +3,7 @@ import {MoreVert} from "@material-ui/icons";
 import {useState, useEffect} from "react";
 import axios from "axios";
 import {format} from "timeago.js";
+import {Link} from "react-router-dom";
 
 export default function Post({post}) {
     const [like, setLike] = useState(post.likes.length);
@@ -27,7 +28,9 @@ export default function Post({post}) {
             <div className="postWrapper">
                 <div className="postTop">
                      <div className="postTopLeft">
+                         <Link to={`profile/${user.username}`}>
                          <img className="postProfileImg" src={user.profilePicture || PF+"person/noAvatar.png"} alt=""/>
+                         </Link>
                          <span className="postUsername">{user.username}</span>
                          <span className="postDate">{format(post.createdAt)}</span>
                     </div> 

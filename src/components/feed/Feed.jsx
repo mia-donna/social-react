f6979248529082dd1120d412c1751a545626c138
Mia Allen <mia@vidops.io>
2021-08-10 17:43:12 +0100

    adding fullstack

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,21 +1,28 @@
 import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import { Posts } from "../../dummyData";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 export default function Feed() {
+    const [posts, setPosts] = useState([]);
+
+
+    useEffect(()=>{
+        const fetchPosts = async () =>{
+            const res = await axios.get("posts/timeline/610abd56bf9ed33e62d4149c");
+            setPosts(res.data)
+        };
+       fetchPosts();
+    }, []);
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
-                {Posts.map((p) => (
-                 <Post key={p.id}post={p}/>
-                ))}
-                
-                
-               
-
+            {posts.map((p) => (
+                <Post key={p.id}post={p}/>
+            ))}
             </div>
         </div>
     )
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,22 @@
 import "./post.css";
 import {MoreVert} from "@material-ui/icons";
-import {Users} from "../../dummyData";
-import {useState} from "react";
-
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 
 export default function Post({post}) {
-    const [like, setLike] = useState(post.like)
-    const [isLiked, setIsLiked] = useState(false)
+    const [like, setLike] = useState(post.likes.length);
+    const [isLiked, setIsLiked] = useState(false);
+    const [user, setUser] = useState({});
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+    useEffect(()=>{
+        const fetchUser = async () =>{
+            const res = await axios.get(`users/${post.userId}`);
+            setUser(res.data)
+        };
+       fetchUser();
+    }, []);
 
     const likeHandler =()=>{
         setLike(isLiked ? like-1 : like+1)
@@ -19,8 +27,8 @@ export default function Post({post}) {
             <div className="postWrapper">
                 <div className="postTop">
                      <div className="postTopLeft">
-                         <img className="postProfileImg" src="/assets/person/1.jpeg" alt=""/>
-                         <span className="postUsername">{Users.filter((u) =>  u.id === post.userId)[0].username}</span>
+                         <img className="postProfileImg" src={user.profilePicture || PF+"person/noAvatar.png"} alt=""/>
+                         <span className="postUsername">{user.username}</span>
                          <span className="postDate">{post.date}</span>
                     </div> 
                      <div className="postTopRight">

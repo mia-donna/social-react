8b121690273ebe8b0f8dddac91f98a8d588b2bcd
Mia Allen <mia@vidops.io>
2021-08-10 12:01:33 +0100

    starting full-stack

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+
 
 
 
 
 function App() {
-  return <Home/>
+  return <Profile/>
 }
 
 export default App;
